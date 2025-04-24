1a0beab8bbdec485527522995146fcd903886a66
Devloper-sudo <abhi190291@gmail.com>
2025-04-24 12:18:34 +0530

    Initial commit

---
+++ lib/login_placeholder.dart
@@ -0,0 +1,14 @@
+// Placeholder for production (when using .aar)
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreenPlaceholder extends StatelessWidget {
+  const LoginScreenPlaceholder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('LoginScreen from Module Package')),
+    );
+  }
+}
